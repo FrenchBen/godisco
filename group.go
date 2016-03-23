154b2d323c0f00124076f8986038da54c004c623
French Ben <me+git@frenchben.com>
2016-03-23 15:33:19 -0700

    Updated group membership and update logic
    Signed-off-by: French Ben <me+git@frenchben.com>

--- group.go
+++ group.go
@@ -3,15 +3,52 @@ package godisco
 import (
 	"encoding/json"
 	"fmt"
-	"strings"
 )
 
 // GroupResponse expected struct for groups
-type GroupResponse struct{}
+type GroupResponse struct {
+	Basic struct {
+		ID          int    `json:"id"`
+		Automatic   bool   `json:"automatic"`
+		Name        string `json:"name"`
+		UserCount   int    `json:"user_count"`
+		AliasLevel  int    `json:"alias_level"`
+		Visible     bool   `json:"visible"`
+		Domain      string `json:"automatic_membership_email_domains"`
+		Retroactive bool   `json:"automatic_membership_retroactive"`
+		Primary     bool   `json:"primary_group"`
+		Title       string `json:"title"`
+		TrustLevel  int    `json:"grant_trust_level"`
+		Messages    bool   `json:"has_messages"`
+		Mentionable bool   `json:"mentionable"`
+	} `json:"basic_group"`
+}
+
+// GroupMembersResponse defines list of members in a group
+type GroupMembersResponse struct {
+	Members []Member `json:"members"`
+	Owners  []Member `json:"owners"`
+	Meta    struct {
+		Total  int `json:"total"`
+		Limit  int `json:"limit"`
+		Offset int `json:"offset"`
+	}
+}
+
+// Member information about a member
+type Member struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Avatar   string `json:"avatar_template"`
+	Name     string `json:"name"`
+	Title    string `json:"title"`
+	Posted   string `json:"last_posted_at"`
+	Seen     string `json:"last_seen_at"`
+}
 
 // GetGroup show details of a given group
-func GetGroup(req Requester, group string) (groupInfo *GroupResponse, err error) {
-	endpoint := fmt.Sprintf("/groups/%s.json", group)
+func GetGroup(req Requester, groupName string) (groupInfo *GroupResponse, err error) {
+	endpoint := fmt.Sprintf("/groups/%s.json", groupName)
 	body, _, err := req.Get(endpoint)
 	if err != nil {
 		return nil, err
@@ -21,14 +58,14 @@ func GetGroup(req Requester, group string) (groupInfo *GroupResponse, err error)
 }
 
 // GetGroupMembers list members of a given group
-func GetGroupMembers(req Requester, groupName string) (groupInfo *GroupResponse, err error) {
+func GetGroupMembers(req Requester, groupName string) (groupMemberInfo *GroupMembersResponse, err error) {
 	endpoint := fmt.Sprintf("/groups/%s/members.json", groupName)
 	body, _, err := req.Get(endpoint)
 	if err != nil {
 		return nil, err
 	}
-	err = json.Unmarshal(body, &groupInfo)
-	return groupInfo, err
+	err = json.Unmarshal(body, &groupMemberInfo)
+	return groupMemberInfo, err
 }
 
 //@TODO
@@ -37,10 +74,8 @@ func GetGroupMembers(req Requester, groupName string) (groupInfo *GroupResponse,
 // alias_level = 0
 
 type groupUpdate struct {
-	Name       string `json:"group[name]"`
-	AliasLevel int    `json:"group[alias_level]"`
-	Visible    bool   `json:"group[visible]"`
-	Users      string `json:"group[usernames]"`
+	GroupID string   `json:"group_id"`
+	Users   []string `json:"users"`
 }
 
 // GroupInfo describes the group update received
@@ -66,13 +101,11 @@ type GroupInfo struct {
 
 func updateGroupMembers(req Requester, groupName string, groupID string, members []string) (groupInfo *GroupInfo, err error) {
 	update := &groupUpdate{
-		Name:       groupName,
-		AliasLevel: 0,
-		Visible:    false,
-		Users:      strings.Join(members, ","),
+		GroupID: groupID,
+		Users:   members,
 	}
 	data, err := json.Marshal(update)
-	endpoint := fmt.Sprintf("/admin/groups/%s", groupID)
+	endpoint := "/admin/groups/bulk"
 	body, _, err := req.Post(endpoint, data)
 	if err != nil {
 		return nil, err
