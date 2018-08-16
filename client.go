a28d2d686c10edf6044e9ff04d6980de9767cecd
Elton Minetto <eminetto@gmail.com>
2018-08-16 17:59:02 -0300

    Add new functions

--- client.go
+++ client.go
@@ -4,11 +4,10 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
-
-	"github.com/Sirupsen/logrus"
 )
 
 // Requester interface allowing testing further down the line
@@ -16,6 +15,7 @@ type Requester interface {
 	do(*http.Request) ([]byte, int, error)
 	Get(string) ([]byte, int, error)
 	Post(string, []byte) ([]byte, int, error)
+	Put(string, []byte) ([]byte, int, error)
 }
 
 // Client struct to keep track of new client
@@ -48,7 +48,6 @@ func NewClient(ClientEndpoint string, ClientKey string, ClientUser string) (*Cli
 // Get resource string
 func (c *Client) Get(resource string) ([]byte, int, error) {
 	url := fmt.Sprintf("%s%s", c.domain, resource)
-	logrus.Debugf("URL: %v", url)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, 0, err
@@ -67,26 +66,36 @@ func (c *Client) do(req *http.Request) ([]byte, int, error) {
 	if err != nil {
 		return nil, 0, err
 	}
-	logrus.Debugf("req Body: %v", body)
-	logrus.Debugf("Status: %v", resp.StatusCode)
-	logrus.Debugf("Err: %v", err)
 	if resp.StatusCode != 200 && resp.StatusCode != 404 {
 		err = fmt.Errorf("Received unexpected status %d while trying to retrieve the server data with \"%s\"", resp.StatusCode, string(body))
 		return nil, resp.StatusCode, err
 	}
-	logrus.Debug(string(body))
 	return body, resp.StatusCode, nil
 }
 
 // Post to resource string the data provided
 func (c *Client) Post(resource string, data []byte) ([]byte, int, error) {
-	logrus.Debugf("POST Data: %v", string(data))
 	apiAuth := url.Values{}
 	apiAuth.Set("api_key", c.key)
 	apiAuth.Add("api_username", c.user)
 	url := fmt.Sprintf("%s%s?%s", c.domain, resource, apiAuth.Encode())
-	logrus.Debugf("URL: %v", url)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		return nil, 0, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	return c.do(req)
+}
+
+// Put to resource string the data provided
+func (c *Client) Put(resource string, data []byte) ([]byte, int, error) {
+	log.Printf("PUT Data: %v", string(data))
+	apiAuth := url.Values{}
+	apiAuth.Set("api_key", c.key)
+	apiAuth.Add("api_username", c.user)
+	url := fmt.Sprintf("%s%s?%s", c.domain, resource, apiAuth.Encode())
+	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(data))
 	if err != nil {
 		return nil, 0, err
 	}
